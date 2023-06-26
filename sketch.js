e51e23e3a5a28c4abd34af96efc795262955c8a8
maricassia <137062293+maricassia@users.noreply.github.com>
2023-06-26 08:16:52 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400,400);
+  background(color(100,0,0));
+  
+  cor = color(random(255), random(255), random(255));
+  posicaoHorizontal = 0;
+  posicaoVertical = random(height);
+  
+  posicaoHorizontal2 = 0;
+  posicaoVertical2 = random(height);
+}
+
+function draw() {
+ fill(cor);
+circle(posicaoHorizontal,posicaoVertical,50);
+circle(posicaoHorizontal2,posicaoVertical2);
+posicaoHorizontal+=random(0,3);
+posicaoVertical+=random(-3,3);
+posicaoHorizontal2+=random(0,3);
+posicaoVertical2+=random(-3,3);
+if(mouseIsPressed) {
+cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+}
+}
